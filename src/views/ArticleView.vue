45acab7b9180341430d8ba705feec6ad9c530ed6
gunawan480 <ahmadsyahgunawan$@gmail.com>
2025-07-07 18:37:50 +0700

    🎨 Ubah garis kuning menjadi biru di detail artikel

--- src/views/ArticleView.vue
+++ src/views/ArticleView.vue
@@ -1,22 +1,21 @@
 <template>
-    <div class="w-full md:w-3/5 h-20 mx-auto md:mt-5">
-        <div class="bg-white rounded-xl mx-3 p-5 md:p-10 md:mx-0">
-            <div>
-                <h1 class="text-xl md:text-4xl text-black text-left font-bold leading-relaxed">{{ title }}</h1>
-                <div class="mt-3 text-left text-gray-800 text-sm">Published at <span>{{ date }}</span></div>
-                <div class="h-[2px] w-20 my-5 md:my-10 bg-[#ffdb70] md:w-1/3 aos-init aos-animate mr-2"></div>
-                <div>
-                    <div class="relative w-full" style="padding-top: 50%;">
-                        <img :src="image" class="absolute top-0 left-0 rounded-lg w-full h-full object-cover"
-                            alt="Thumbnail">
-                    </div>
-                </div>
-                <div class="text-left text-black mt-8" v-html="content">
-                    
-                </div>
-            </div>
+  <div class="w-full md:w-3/5 h-20 mx-auto md:mt-5">
+    <div class="bg-white rounded-xl mx-3 p-5 md:p-10 md:mx-0">
+      <div>
+        <h1 class="text-xl md:text-4xl text-black text-left font-bold leading-relaxed">{{ title }}</h1>
+        <div class="mt-3 text-left text-gray-800 text-sm">Published at <span>{{ date }}</span></div>
+        <!-- ⬇️ GANTI warna baris ini -->
+        <div class="h-[2px] w-20 my-5 md:my-10 bg-blue-400 md:w-1/3 aos-init aos-animate mr-2"></div>
+        <div>
+          <div class="relative w-full" style="padding-top: 50%;">
+            <img :src="image" class="absolute top-0 left-0 rounded-lg w-full h-full object-cover" alt="Thumbnail">
+          </div>
         </div>
+
+        <div class="text-left text-black mt-8" v-html="content"></div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 import axios from 'axios';
